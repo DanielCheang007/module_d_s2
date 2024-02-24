d03571d06dfe86ee5a51ef0ae52117d017132c86
stevencpttm <56670584+stevencpttm@users.noreply.github.com>
2024-02-24 10:40:11 +0800

    setup header and main body

---
+++ src/views/HeaderView.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+    <header>
+        <h1>WorldSkills: Games</h1>
+
+        <div class="user-utils">
+            <nav>
+                <RouterLink to="/">Sign Up</RouterLink>
+                <RouterLink to="/about">Sign In</RouterLink>
+            </nav>
+        </div>
+    </header>
+</template>
---
+++ src/views/MainView.vue
@@ -0,0 +1,5 @@
+<template>
+    <main>
+        This is the main body
+    </main>
+</template>
