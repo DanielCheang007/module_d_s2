6127f97731eb4e7aca94c7e2d22652ccbbc41b80
DanielCheang007 <93252061+DanielCheang007@users.noreply.github.com>
2024-03-02 12:14:48 +0800

    add sign in component

--- src/views/auth/SignIn.vue
+++ src/views/auth/SignIn.vue
@@ -1,5 +1,103 @@
+<script setup>
+import { ref } from 'vue'
+
+const API_URL = import.meta.env.VITE_API_URL
+
+// blank template for form and error messge
+const blankTmpl = {
+    username: null,
+    password: null
+}
+
+const formData = ref({...blankTmpl})
+
+const errors = ref(null)
+
+const result = ref(null)
+
+async function signin() {
+
+    // clear error messages
+    errors.value = null
+
+    const url = API_URL + '/auth/signin'
+
+    const res = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData.value)
+    })
+
+    const json = await res.json()
+
+    if (json.status === 'invalid') {
+        errors.value = json?.message
+    }
+
+    result.value = json
+
+    formData.value = {...blankTmpl}
+}
+
+</script>
+
 <template>
     <div class="page-header">
         <h1>Sign In</h1>
     </div>
+    <div class="page-content">
+        <div>
+            {{ formData }}
+        </div>
+        <div>
+            {{ result }}
+        </div>
+        <form @submit.prevent.stop="signin">
+            <div class="error">{{ errors }}</div>
+            <div>
+                <label>
+                    <span>@</span>
+                    <input type="text" placeholder="username" v-model="formData.username">
+                </label>
+            </div>
+            <div class="field">
+                <label>
+                    <span>#123</span>
+                    <input type="password" placeholder="password" v-model="formData.password">
+                </label>
+            </div>
+            <div class="actions">
+                <input type="submit" value="Sign In">
+                <a href="#" @click.prevent="cancel">Cancel</a>
+            </div>
+        </form>
+    </div>
 </template>
+
+<style scoped>
+    form {
+        width: 300px;
+        margin: 0 auto;
+    }
+    label {
+        display: inline-flex;
+    }
+    label > span {
+        background: #ccc;
+        width: 3rem;
+    }
+
+    .field {
+        margin: .5rem 0;
+    }
+
+    .actions a {
+        margin-left: 6rem;
+    }
+
+    .error {
+        color: red;
+    }
+</style>
